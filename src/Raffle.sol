9b455121c76ace07df882a4cc596334bcd633cfa
Vention <backstreetal6year@gmail.com>
2024-06-04 16:47:57 +0800

    VRF2-5 starting

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,7 +1,11 @@
 //SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.25;
-contract Raffle {
+
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+
+contract Raffle is VRFConsumerBaseV2Plus{
     //We have to store the Player enter the raffle
     //address[] payable private s_player;
     address payable[] private s_palyerList;
@@ -20,5 +24,23 @@ contract Raffle {
         }else {
             s_palyerList.push(payable(msg.sender));
         }
+    }
+
+    function getTheRequestIdAndKickOffRequestRandomWords() internal returns (uint256 requestId) {
+        // Will revert if subscription is not set and funded.
+        requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: keyHash,
+                subId: s_subscriptionId,
+                requestConfirmations: requestConfirmations,
+                callbackGasLimit: callbackGasLimit,
+                numWords: numWords,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    VRFV2PlusClient.ExtraArgsV1({
+                        nativePayment: enableNativePayment
+                    })
+                )
+            })
+        );
     }
 }
--- test/Raffle.t.sol
+++ test/Raffle.t.sol
@@ -1,6 +1,6 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
 contract RaffleTest {
-    
+
 }
