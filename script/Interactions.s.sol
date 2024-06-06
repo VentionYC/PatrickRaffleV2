29ffb537c2d2e68398fcdf6008924e48550a9e58
Vention <backstreetal6year@gmail.com>
2024-06-06 18:01:22 +0800

    add interaction script

---
+++ script/Interactions.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+import {Script} from "forge-std/Script.sol";
+
+contract CreateChainlinkVRFSub is Script(){
+    
+}
+
+contract FundChainlinkVRFSub is Script(){
+
+}
+
+contract AddConsumerToChainlinkVRFSub is Script(){
+
+}
